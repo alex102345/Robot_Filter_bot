6402a92f1effc8f27034388ec1ae13330ac4ceb5
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-07-08 11:25:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyniteOfficial/Auto-Filter-Bot /Ajax
+  git clone https://github.com/alex102345/Robot_Filter_bot /Robot_Filter_bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Zsearcher
+  git clone $UPSTREAM_REPO /Robot_Filter_bot
 fi
-cd /Zsearcher
+cd /Robot_Filter_bot
 pip3 install -U -r requirements.txt
 echo "Starting Zsearcherbot....🔥"
 python3 bot.py
